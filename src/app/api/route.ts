704da39b2044121bb47dd8ec4f2b30835ce8d402
AkkalDhami <dhamiakkal21@gmail.com>
2026-06-29 12:45:42 +0545

    feat(db): update database connection to use Neon and upgrade drizzle-orm

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,5 +1,6 @@
 import { usersTable } from "@/db";
 import db from "@/db/connection";
+import { eq } from "drizzle-orm";
 import { NextResponse } from "next/server";
 
 export const GET = async () => {
@@ -7,19 +8,31 @@ export const GET = async () => {
     const user: typeof usersTable.$inferInsert = {
       name: "John",
       age: 30,
-      email: "john@example.com"
+      email: "john123@example.com"
     };
+
     await db.insert(usersTable).values(user);
+    console.log("New user created!");
 
     const users = await db.select().from(usersTable);
-    console.log({ users });
+    console.log("Getting all users from the database: ", users);
+    await db
+      .update(usersTable)
+      .set({
+        age: 31
+      })
+      .where(eq(usersTable.email, user.email));
+    console.log("User info updated!");
+
+    await db.delete(usersTable).where(eq(usersTable.email, user.email));
+    console.log("User deleted!");
 
     return NextResponse.json(
       {
         success: true,
         data: users
       },
-      { status: 500 }
+      { status: 200 }
     );
   } catch (error) {
     console.error("Delete error:", error);
--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,6 +1,17 @@
-import { drizzle } from "drizzle-orm/node-postgres";
+// import { drizzle } from "drizzle-orm/node-postgres";
 
-const db = drizzle(process.env.DATABASE_URL!, {
+// const db = drizzle(process.env.DATABASE_URL!, {
+//   logger: process.env.NODE_ENV === "development"
+// });
+
+// export default db;
+
+import { neon } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http";
+
+const sql = neon(process.env.DATABASE_URL!);
+const db = drizzle({
+  client: sql,
   logger: process.env.NODE_ENV === "development"
 });
 
